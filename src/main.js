8c7ec52a0a466573e757c1435324dc2860b0c104
Patryk <patrykowo@gmail.com>
2022-10-18 21:09:53 +0200

    Add masonry to create photo gallery.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
+import { VueMasonryPlugin } from 'vue-masonry';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(VueMasonryPlugin).mount('#app');
