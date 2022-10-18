15be0603f88853ebca8e6194c5777306fb2dc68b
Patryk <patrykowo@gmail.com>
2022-10-18 10:14:36 +0200

    Change home page file name to SearchView.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="app">
-    <router-link to="/">spacer</router-link>
-    <router-link to="/about">about</router-link>
+    <div class="nav">
+      <router-link to="/">spacer</router-link>
+      <router-link to="/about">about</router-link>
+    </div>
     <router-view />
   </div>
 </template>
