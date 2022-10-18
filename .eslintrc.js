6e123a00e18cee10a35f31b59c83cc1fd207bf68
Patryk <patrykowo@gmail.com>
2022-10-18 13:02:55 +0200

    Change eslint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,9 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'linebreak-style': 0,
+    'max-len': 'off',
+    'indent': 'off',
+    'vue/multi-word-component-names': 'off',
   },
   overrides: [
     {
